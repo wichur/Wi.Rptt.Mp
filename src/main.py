29ae751e6e4f5e2c1959a1a9f76a00146ffb964b
Maciej Raczynski <maciej.raczynski@gmail.com>
2021-05-05 19:24:00 +0200

    aaa

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ import secrets
 from app.ota_updater import OTAUpdater
 
 def download_and_install_update_if_available():
-    o = OTAUpdater('https://github.com/wichur/Wi.Rptt.Mp', headers={
-        'Authorization': 'token {}'.format(secrets.TOKEN)}, main_dir='app', secrets_file="secrets.py")
+    o = OTAUpdater('https://github.com/wichur/Wi.Rptt.Mp', headers={main_dir='app', secrets_file="secrets.py")
     o.install_update_if_available_after_boot(
         secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
 
